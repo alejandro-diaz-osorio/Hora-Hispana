f8faae4ff34f817fdba32efcee2e5c794d06776e
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-21 00:09:34 -0500

    Refactor HTML structure and enhance JavaScript functionality for date and time selection

--- main.js
+++ main.js
@@ -1,24 +1,89 @@
-import { toast } from "https://cdn.skypack.dev/wc-toast";
-import { $, setInitialDate } from "./utils.js";
-import countries from "./countries.json";
-import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+import { toast } from "https://cdn.skypack.dev/wc-toast"
+import { setInitialDate } from "./utils.js"
+import countries from "./countries.json"
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill"
 
-polyfillCountryFlagEmojis();
-
-const $input = document.querySelector("input");
-const $result = document.querySelector("#result");
+polyfillCountryFlagEmojis()
 
 function changeTimeZone(date, timeZone) {
-  const dateToUse = typeof date === "string" ? new Date(date) : date;
-  return new Date(dateToUse.toLocaleString("en-US", { timeZone }));
+    const dateToUse = typeof date === "string" ? new Date(date) : date
+
+    return new Date(
+        dateToUse.toLocaleString("en-US", {
+            timeZone,
+        })
+    )
 }
 
 const transformDateToString = (date) => {
-  const localDate = date.toLocaleString("es-CO", {
-    hour12: false,
-    hour: "numeric",
-    minute: "numeric",
-  });
+    const localDate = date.toLocaleString("es-ES", {
+        hour12: false,
+        hour: "numeric",
+        minute: "numeric",
+    })
 
-  return localDate.replace(":00", "H");
-};
+    return localDate.replace(":00", "H")
+}
+
+const $input = document.querySelector("input")
+const $result = document.querySelector("#result")
+
+const fillTextArea = () => {
+    const date = $input.value
+
+    const mainDate = new Date(date)
+
+    const times = {}
+
+    countries.forEach((country) => {
+        const { country_code: code, emoji, timezones } = country
+        const [timezone] = timezones
+
+        const dateInTimezone = changeTimeZone(mainDate, timezone)
+        const hour = dateInTimezone.getHours()
+
+        times[hour] ??= []
+
+        times[hour].push({
+            date: dateInTimezone,
+            code,
+            emoji,
+            timezones,
+        })
+    })
+
+    const sortedTimesEntries = Object.entries(times).sort(
+        ([timeA], [timeB]) => timeB - +timeA
+    )
+
+    const html = sortedTimesEntries
+        .map(([, countries]) => {
+            const flags = countries
+                .map((country) => `${country.emoji}`)
+                .join(" ")
+            const [country] = countries
+            const { date } = country
+
+            return `${transformDateToString(date)} ${flags}`
+        })
+        .join("\n")
+
+    $result.value = html
+}
+
+$input.addEventListener("change", () => {
+    fillTextArea().then(() => {
+        toast("¡Copiado al portapeles!", {
+            icon: {
+                type: "success",
+            },
+        })
+    })
+})
+
+const onLoad = async () => {
+    setInitialDate()
+    fillTextArea()
+}
+
+onLoad()
