d10f940a4c99dfd694d68510a6a9e684d5033b26
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2025-06-23 01:59:30 -0500

    Refactor date handling and improve toast notifications in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import { toast } from "https://cdn.skypack.dev/wc-toast"
-// import { setInitialDate } from "./utils.js"
 import countries from "./countries.json"
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill"
-
 polyfillCountryFlagEmojis()
 
+const $input = document.querySelector("#date")
+const $result = document.querySelector("#result")
+
+const setInitialDate = () => {
+    const now = new Date()
+    now.setMinutes(0, 0, 0)
+    if (new Date().getMinutes() > 0) now.setHours(now.getHours() + 1)
+
+    const local = new Date(now.getTime() - now.getTimezoneOffset() * 60000)
+        .toISOString()
+        .slice(0, 16)
+
+    $input.value = local
+}
 function changeTimeZone(date, timeZone) {
     const dateToUse = typeof date === "string" ? new Date(date) : date
 
@@ -16,17 +28,17 @@ function changeTimeZone(date, timeZone) {
 }
 
 const transformDateToString = (date) => {
-    const localDate = date.toLocaleString("es-ES", {
+    const localDate = date.toLocaleString("en-US", {
+        weekday: "short",
+        month: "2-digit",
+        day: "numeric",
         hour12: false,
         hour: "numeric",
         minute: "numeric",
     })
 
-    return localDate.replace(":00", " h")
+    return localDate.replace(":00", " H")
 }
-
-const $input = document.querySelector("#date")
-const $result = document.querySelector("#result")
 
 const fillTextArea = () => {
     const date = $input.value
@@ -73,18 +85,39 @@ const fillTextArea = () => {
 
 $input.addEventListener("change", () => {
     fillTextArea()
-    toast(`Date changed to ${$input.value}`, {
-        duration: 5000,
-        position: "bottom",
+
+    if ($input.value === "") {
+        const date = new Date(Date.now())
+        $input.value = date.toISOString().slice(0, 16)
+        fillTextArea()
+    }
+
+    toast("Ready to copy", {
+        icon: {
+            type: "custom",
+            content: "📋",
+        },
+        theme: {
+            type: "dark",
+        },
     })
 })
 
-
+$result.addEventListener("click", () => {
+    navigator.clipboard.writeText($result.value)
+    toast("Copied to clipboard", { icon: { type: "success" } })
+})
 
 const onLoad = async () => {
-    $input.value = '2001-09-14T07:00'
+    setInitialDate()
     fillTextArea()
-    toast("App loaded", { icon: { type: "success" }},{duration: 10000})
+    toast("Click on the result to copy", {
+        icon: {
+            type: "custom",
+            content: "⌛",
+        },
+        duration: 2000,
+    })
 }
 
 onLoad()
